b8cca691df2f2db655b700887e952e92cca64e76
diomedescerda <kenmatwofour@gmail.com>
2025-03-05 22:15:08 -0500

    feat(inventory service): init/first version

---
+++ inventory/src/main/java/com/example/inventory/controllers/InventoryController.java
@@ -0,0 +1,51 @@
+package com.example.inventory.controllers;
+
+import com.example.inventory.entities.Inventory;
+import com.example.inventory.services.InventoryService;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/inventories")
+public class InventoryController {
+    private final InventoryService inventoryService;
+
+    public InventoryController(InventoryService inventoryService) {
+        this.inventoryService = inventoryService;
+    }
+
+    @GetMapping
+    public Flux<List<Inventory>> getAllInventories() {
+        return Flux.just(inventoryService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public Mono<Inventory> getInventoryById(@PathVariable("id")  UUID id) {
+        return Mono.justOrEmpty(inventoryService.findById(id));
+    }
+
+    @GetMapping("/product/{productId}")
+    public Mono<Inventory> getInventoryByProductId(@PathVariable("productId")  UUID productId) {
+        return Mono.justOrEmpty(inventoryService.findByProductId(productId));
+    }
+
+    @PostMapping
+    public Mono<Inventory> createInventory(@RequestBody Inventory inventory) {
+        return Mono.justOrEmpty(inventoryService.save(inventory));
+    }
+
+    @PutMapping("/{id}")
+    public Mono<Inventory> updateInventory(@PathVariable("id")  UUID id, @RequestBody Inventory inventory) {
+        return Mono.justOrEmpty(inventoryService.update(id, inventory));
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> deleteInventory(@PathVariable("id")  UUID id) {
+        inventoryService.deleteById(id);
+        return null;
+    }
+}
---
+++ inventory/src/main/java/com/example/inventory/entities/Inventory.java
@@ -0,0 +1,25 @@
+package com.example.inventory.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "inventories")
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private UUID productId;
+
+    @Column(nullable = false)
+    private int quantity;
+}
