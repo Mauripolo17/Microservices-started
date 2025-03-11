b5367ab1dde9187b98999efc24b48b08c733e9f1
LeyLopez <leidislopez2017@gmail.com>
2025-03-11 17:05:00 -0500

    Product Module

--- product/src/main/java/com/example/product/entity/Product.java
+++ product/src/main/java/com/example/product/entity/Product.java
@@ -1,11 +1,20 @@
 package com.example.product.entity;
 
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.UUID;
 
-
+@Data
+@Document(collection = "products")
 public class Product {
+
+    @Id
+    private UUID id;
+    private String name;
+    private String description;
+    private Double price;
 
 }
