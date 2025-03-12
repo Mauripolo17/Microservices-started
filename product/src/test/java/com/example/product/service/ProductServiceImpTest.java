acfd5b6c01807baaeaa1dfffb224e63b187a2e25
Mauricio Polo <mauriciopoloem@gmail.com>
2025-03-12 16:45:36 -0500

    fix bug

---
+++ product/src/test/java/com/example/product/service/ProductServiceImpTest.java
@@ -0,0 +1,40 @@
+package com.example.product.service;
+
+import com.example.product.entity.Product;
+import com.example.product.repository.ProductRepository;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceImpTest {
+
+    private ProductServiceImp productServiceImp;
+    private Product product;
+    private ProductRepository productRepository;
+
+    @BeforeEach
+    void setup(){
+
+    }
+    @Test
+    void getProductById() {
+    }
+
+    @Test
+    void getAllProducts() {
+    }
+
+    @Test
+    void deleteProduct() {
+    }
+
+    @Test
+    void saveProduct() {
+    }
+
+    @Test
+    void updateProduct() {
+    }
+}
