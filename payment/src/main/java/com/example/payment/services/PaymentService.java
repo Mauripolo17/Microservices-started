55620e8e2f05feaca856f7d90a837cbf17615c8a
Mauripolo17 <mauriciopoloem@gmail.com>
2025-03-11 21:40:59 -0500

    Added services and controllers to manage business logic and application routes,

--- payment/src/main/java/com/example/payment/services/PaymentService.java
+++ payment/src/main/java/com/example/payment/services/PaymentService.java
@@ -3,12 +3,14 @@ package com.example.payment.services;
 import com.example.payment.entities.Payment;
 
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface PaymentService {
 
     Optional<Payment> getPaymentById(UUID id);
+    List<Payment> getAllPayments();
     Payment createPayment(Payment payment);
     Optional<Payment> updatePayment(Payment payment, UUID id);
     void deletePayment(UUID id);
