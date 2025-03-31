839faee6d5bfb219af807b2fd308e4f59f4c66be
diomedescerda <kenmatwofour@gmail.com>
2025-03-31 16:07:16 -0500

    feat(circuitBreaker): it works for all of them

--- gateway/src/main/java/com/example/gateway/config/GatewayConfig.java
+++ gateway/src/main/java/com/example/gateway/config/GatewayConfig.java
@@ -13,17 +13,34 @@ public class GatewayConfig {
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder, LoggingFilter loggingFilter) {
         return builder.routes()
                 .route("product-service", r -> r.path("/api/product/**")
+                        .filters(f -> f
+                                .circuitBreaker(config -> config
+                                        .setName("productCircuitBreaker")
+                                        .setFallbackUri("forward:/serviceFallback/Product"))
+                        )
                         .uri("lb://PRODUCT"))
                 .route("order-service", r -> r.path("/api/orders/**")
+                        .filters(f -> f
+                                .filter(loggingFilter.apply(new LoggingFilter.Config()))
+                                .circuitBreaker(config -> config
+                                        .setName("orderCircuitBreaker")
+                                        .setFallbackUri("forward:/serviceFallback/Order"))
+                        )
                         .uri("lb://ORDER"))
                 .route("payment-service", r -> r.path("/api/payment/**")
+                        .filters(f -> f
+                                .filter(loggingFilter.apply(new LoggingFilter.Config()))
+                                .circuitBreaker(config -> config
+                                        .setName("paymentCircuitBreaker")
+                                        .setFallbackUri("forward:/serviceFallback/Payment"))
+                        )
                         .uri("lb://PAYMENT"))
                 .route("inventory-service", r -> r.path("/api/inventories/**")
                         .filters(f -> f
                                 .filter(loggingFilter.apply(new LoggingFilter.Config()))
                                 .circuitBreaker(config -> config
                                         .setName("inventoryCircuitBreaker")
-                                        .setFallbackUri("forward:/inventoryFallback"))
+                                        .setFallbackUri("forward:/serviceFallback/Inventory"))
                                 )
                         .uri("lb://INVENTORY"))
                 .build();
